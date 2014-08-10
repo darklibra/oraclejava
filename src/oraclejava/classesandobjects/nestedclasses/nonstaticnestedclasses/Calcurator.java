3bd716231b4abef13e985b5797ab3bc293432054
kry <darkilbra25@gmail.com>
2014-08-11 00:35:46 +0900

    ..

---
+++ src/oraclejava/classesandobjects/nestedclasses/nonstaticnestedclasses/Calcurator.java
@@ -0,0 +1,23 @@
+package oraclejava.classesandobjects.nestedclasses.nonstaticnestedclasses;
+
+public class Calcurator {
+	private int l;
+	private int r;
+	
+	public Calcurator(int l, int r) {
+		this.l = l;
+		this.r = r;
+	}
+	
+	public class Result {
+		public int add() {return l + r;}
+		public int sub() {return l - r;}
+	}
+	
+	public static void main(String[] args) {
+		Result r = new Calcurator(10, 5).new Result();
+		
+		System.out.println(" add " + r.add());
+		System.out.println(" sub " + r.sub());
+	}
+}
