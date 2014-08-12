c556c20d9542c5347a0602950c5438c0919fa7be
dain <darklibra25@gmail.com>
2014-08-12 15:53:47 +0900

    중첩클래스 예제 수정

--- src/oraclejava/classesandobjects/nestedclasses/staticnestedclasses/Member.java
+++ src/oraclejava/classesandobjects/nestedclasses/staticnestedclasses/Member.java
@@ -7,10 +7,12 @@ public class Member {
 	private Member() {}
 	
 	public static class MemberParser {
-		public static Member paser() {
+		// TODO : 인자를 받아서 잘라서 보내주는 코드를 추가 예정
+		public static Member parser(String data) {
+			String[] datas = data.split("#");
 			Member m = new Member();
-			m.name = "test";
-			m.age = 10;
+			m.name = datas[0];
+			m.age = Integer.valueOf(datas[1]);
 			return m;
 		}
 	}
@@ -23,11 +25,18 @@ public class Member {
 		return age;
 	}	
 	
+	@Override
+	public String toString() {
+		return name + "#" + age;
+	}
+
 	public static void main(String[] args) {
-		MemberParser mp = new MemberParser();
-		Member m = mp.paser();
+		String data = "Values#100";
+		Member m = MemberParser.parser(data);
 		
 		System.out.println("name : " + m.getName());
 		System.out.println("age : " + m.getAge());
+		
+		System.out.println("data(" + data + ") == m(" + m + ") ? " + data.equals(m.toString()));
 	}
 }
